e921da799aaa9ef48d2f3af01b1466ac7b5bb969
Federico Di Pierro <nierro92@gmail.com>
2022-03-03 18:22:28 +0100

    update(build): updated libs version to Falco 0.31.1 libs version.
    Signed-off-by: Federico Di Pierro <nierro92@gmail.com>

--- cmake/modules/falcosecurity-libs.cmake
+++ cmake/modules/falcosecurity-libs.cmake
@@ -29,8 +29,8 @@ else()
   # default below In case you want to test against another falcosecurity/libs version just pass the variable - ie., `cmake
   # -DFALCOSECURITY_LIBS_VERSION=dev ..`
   if(NOT FALCOSECURITY_LIBS_VERSION)
-    set(FALCOSECURITY_LIBS_VERSION "423235173187cd4629f1e1fd6409a013a9a3fb84")
-    set(FALCOSECURITY_LIBS_CHECKSUM "SHA256=13a93fe04a328966a5fa34f7b6beb10fad94a0d02aef14804e5d033acd22fad3")
+    set(FALCOSECURITY_LIBS_VERSION "b7eb0dd65226a8dc254d228c8d950d07bf3521d2")
+    set(FALCOSECURITY_LIBS_CHECKSUM "SHA256=0f6dcdc3b94243c91294698ee343806539af81c5b33c60c6acf83fc1aa455e85")
   endif()
 
   # cd /path/to/build && cmake /path/to/source
